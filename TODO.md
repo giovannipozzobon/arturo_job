b25b356792750b867b479e0e6fa0aa24bcaa0f24
Paul Robson <paul@robsons.org.uk>
2025-02-07 14:56:43 +0000

    Rethinking yielding code

--- TODO.md
+++ TODO.md
@@ -3,3 +3,4 @@
 Outstanding for FORTH bootable :
 
 - Remake it a stand alone system.
+- Redo tick code completely and port to FORTH , test app etc.
--- applications/default/start.c
+++ applications/default/start.c
@@ -18,4 +18,23 @@ void FTH_ApplicationRun(void);
  */
 void ApplicationRun(void) {
     FTH_ApplicationRun();
+    // char inputLine[64];
+    // while (1) {
+    //     VDUWrite(42);VDUWrite(32);
+    //     int linePos = 0;
+    //     int c;
+    //     while (c = KBDGetKey(),c != 13) {
+    //         if (c >= ' ' && c < 0x7F && linePos < sizeof(inputLine)-1) {
+    //             c = tolower(c);
+    //             inputLine[linePos++] = c;
+    //             VDUWrite(c);
+    //         }
+    //         if (HASTICK50_FIRED()) {                                                    // Time to do a 50Hz tick (Don't use this for timing !)
+    //             TICK50_RESET();                                                         // Reset the tick flag
+    //             if (USBUpdate() == 0) return;                                           // Update USB, exit simulator
+    //             KBDCheckTimer();                                                        // Check for keyboard repeat
+    //         }
+    //     }
+    //     VDUWrite(13);VDUWrite(10);
+    // }
 }
